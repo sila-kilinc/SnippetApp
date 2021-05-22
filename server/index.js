89439a2abb6d93372119dcf0383f5fdd876b5cf0
sila-kilinc <sila.kilinc@ceng.deu.edu.tr>
2021-05-22 22:04:24 +0300

    create

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+
+const app = express();
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Snippet App");
+});
+
+
+const PORT = process.env.PORT || 5000;
+const CONNECTION_URL= "mongodb+srv://web_project:proje12345_@cluster0.iefo5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT} `));
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
+
+
